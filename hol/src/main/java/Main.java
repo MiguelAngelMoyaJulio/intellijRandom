7f64649eb49c77e1c642883c22c44e02b13eb7c8
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-05-22 23:12:35 -0300

    function sum added

--- hol/src/main/java/Main.java
+++ hol/src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("jsjsj");
+        System.out.println(suma(5, 5));
+
+    }
+
+    private static int suma(int a,int b) {
+        return (a + b);
     }
 }
